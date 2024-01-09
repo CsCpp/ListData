0e0ff31abd7fa3752c3377967df2697dd1dae11a
CppKotlin <cscppkotlin@gmail.com>
2024-01-09 22:33:36 +0300

    Уалил

--- ListData/ListData.cpp
+++ ListData/ListData.cpp
@@ -3,42 +3,75 @@
 using namespace std;
 
 template<typename T>
-class List {
+class List {                            //Класс СПИСОК
 public:
     List();
     ~List();
-    void push_back(T data);
-    int GetSize() 
+    void pop_front();                   //удалить первый элемент в списке
+    void push_back(T data);             //добавить данные в конец списка
+    void clear();                       //очистить список
+    int GetSize()                       //отдает количество элементов
+
     {
         return Size;
     };
+    void push_front(T data);
+    T& operator[](const int index);     //перезагрузка оператора []
 
 
 private:
     template<typename T>
-    class Node {
+    class Node {                        // класс с данными
     public:
-        Node* pNext;
-        T data;
+        Node* pNext;                    //указатель на следующие данные в списке
+        T data;                         //данные
         Node(T data=T(), Node* pNext = nullptr)
         {
             this->data = data;
             this->pNext = pNext;
         }
     };
-    int Size;
-    Node<T> *head;
+    int Size;                           //количество элементов в списке
+    Node<T> *head;                      //указатель на первый 
 };
+
+
 template<typename T>
 List<T>::List()
 {
     Size = 0;
     head = nullptr;
 }
+
 template<typename T>
 List<T>::~List()
 {
+    cout << "Вызвался деструктор "<< endl;
+    clear();
 }
+
+template<typename T>
+void List<T>::pop_front()
+{
+    Node<T> *temp = head;       //указатель temp хранит адрес первого элемента 
+    head = head->pNext;         //указатель head хранит адрес второго элемента 
+    delete temp;                //удаляем данные по адресу указателя temp 
+    Size--;
+}
+
+template<typename T>
+void List<T>::clear() 
+{
+    while (Size) pop_front();
+}
+template<typename T>
+void List<T>::push_front(T data) 
+{
+    head = new Node<T>(data, head);
+    Size++;
+}
+
+
 
 template<typename T>
 void List<T>::push_back(T data)
@@ -49,7 +82,7 @@ void List<T>::push_back(T data)
     }
     else 
     {
-        Node<T>* current = this->head;
+        Node<T>* current = this->head; // временный указатель на текущий адрес в списке
         while (current->pNext != nullptr) 
         {
             current=current->pNext;
@@ -59,6 +92,24 @@ void List<T>::push_back(T data)
     Size++;
 }
 
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+    int counter = 0;                //текущая позиция в списке
+    Node<T>* current = this->head;  // временный указатель на текущий адрес в списке
+    while (current != nullptr) 
+    {
+        if (counter == index)
+        {
+            return current->data;
+        }
+        current = current->pNext;
+        counter++;
+
+    }
+    
+   }
+
 
 
 int main()
@@ -67,27 +118,38 @@ int main()
 
 
     List<int>lst;
-    cout << lst.GetSize() << endl;
-    lst.push_back(5);
-    cout << lst.GetSize() << endl;
-    lst.push_back(4);
-    cout << lst.GetSize() << endl;
-    lst.push_back(56);
-    cout << lst.GetSize() << endl;
+ /*   int numberCount;
+    cin >> numberCount;
 
 
+    for (int i = 0; i < numberCount; i++)
+    {
+        lst.push_back(rand() % 90);
+    }
+
+    for (int i = 0; i < lst.GetSize(); i++)
+    {
+        cout << lst[i] << endl;
+    }*/
+  /*  cout << "Начинаем удалять элементы" << endl;
+    while ( lst.GetSize())
+    {
+        lst.pop_front();
+        cout <<"Количество элементов = " << lst.GetSize() << endl;
+    }*/
+    lst.clear();
+    lst.push_front(34);
+    lst.push_front(12);
+    lst.push_front(32);
+    lst.push_front(75);
+    lst.push_front(125);
+
+    for (int i = 0; i < lst.GetSize(); i++)
+    {
+        cout << lst[i] << endl;
+        cout << lst[i] << endl;
+    }
 
     return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
 
